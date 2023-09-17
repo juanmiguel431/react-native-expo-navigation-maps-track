2db74dab14a2c0bf65ab7655658ceb6f50d24d33
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-17 17:03:55 -0400

    AccountScreen refactored.

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,17 +1,26 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { AuthContext } from '../context/AuthContext';
+import { Button } from '@rneui/themed';
+import Spacer from '../components/Spacer';
+import { AccountScreenProps } from '../models/screen';
 
-export const AccountScreen: React.FC = () => {
-  const { signOut } = useContext(AuthContext);
+export const AccountScreen: React.FC<AccountScreenProps> = () => {
+  const { state, signOut } = useContext(AuthContext);
   return (
     <View>
-      <Text style={{ fontSize: 48 }} >AccountScreen</Text>
-      <Button title="Sign Out" onPress={signOut} />
+      <Text style={styles.user} >{state.user?.email}</Text>
+      <Spacer>
+        <Button title="Sign Out" onPress={signOut} />
+      </Spacer>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  user: {
+    fontSize: 20
+  }
+});
 
 export default AccountScreen;
