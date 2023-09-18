4585df4e35cd6dee8a0179ec7a5e1772e2ffa6cb
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-17 21:12:49 -0400

    react-native-maps installed.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 export const Map: React.FC = () => {
+
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    points.push({
+      latitude: 37.33233 + i * 0.001,
+      longitude: -122.03121 + i * 0.001
+    });
+  }
+
   return (
-    <Text>I am a Map</Text>
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      <Polyline
+        coordinates={points}
+      />
+    </MapView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default Map;
