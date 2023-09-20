cf7fda8b39ef6c21465a53d43bbfbb778e37cee0
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-19 20:53:00 -0400

    Putting a circle in the map.

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { ICoordinate } from '../models/track';
 
 type MapProps = {
@@ -9,35 +9,26 @@ type MapProps = {
 
 export const Map: React.FC<MapProps> = ({ coords}) => {
 
-  // let points = [];
-  // for (let i = 0; i < 20; i++) {
-  //   points.push({
-  //     latitude: 37.33233 + i * 0.001,
-  //     longitude: -122.03121 + i * 0.001
-  //   });
-  // }
-
   return (
     <MapView
       style={styles.map}
       initialRegion={{
         ...coords,
-        // latitude: 37.33233,
-        // longitude: -122.03121,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-      region={{
-        ...coords,
-        // latitude: 37.33233,
-        // longitude: -122.03121,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
-      }}
+      // region={{
+      //   ...coords,
+      //   latitudeDelta: 0.01,
+      //   longitudeDelta: 0.01,
+      // }}
     >
-      {/*<Polyline*/}
-      {/*  coordinates={points}*/}
-      {/*/>*/}
+      <Circle
+        center={coords}
+        radius={30}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
     </MapView>
   );
 };
